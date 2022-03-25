f03d5021a4106a40d5708b2f6635c6a69aded2fb
Mateusz Pawlikowski (MTWP) <mtwp@demant.com>
2022-03-25 10:43:39 +0100

    Add project files.

---
+++ FChess.Core/ChessInfo.fs
@@ -0,0 +1,28 @@
+﻿namespace FChess.Core
+
+open System.Collections.Generic
+
+module ChessInfo =
+
+    type ChessSide = White | Black
+
+    type ChessPieceInfo = {
+        x : int
+        y : int
+        side : ChessSide
+        moves : ChessboardInfo -> (int * int) list
+    }
+    
+    and ChessPiece =
+    | Pawn of ChessPieceInfo
+    | Knight of ChessPieceInfo
+    | Bishop of ChessPieceInfo
+    | Rook of ChessPieceInfo
+    | Queen of ChessPieceInfo
+    | King of ChessPieceInfo
+    
+    and ChessboardInfo = {
+        length : int
+        width : int
+        chessPieces : HashSet<ChessPiece>
+    }
---
+++ FChess.Core/ChessPieceBuilder.fs
@@ -0,0 +1,19 @@
+﻿namespace FChess.Core
+
+open System.Collections.Generic
+open ChessInfo
+
+module ChessPieceBuilder = 
+
+    type ChessPieceType = Pawn | Knight | Bishop | Rook | Queen | King
+    
+    let GetMoveRule chessPieceType =
+        fun chessboard -> [(0, 0)]
+
+    let BuildChessPiece (x, y) side chessPieceType =
+        let moveRule = GetMoveRule chessPieceType
+        match chessPieceType with
+        | Pawn -> ChessPiece.Pawn({ x = x; y = y; side = side; moves = moveRule })
+    
+    //let DefaultChessPiece side =
+        
