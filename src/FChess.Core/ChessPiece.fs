cc43865e7d351e3fc331c601311ff7bdbfc164d3
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-12-19 18:54:23 +0100

    ChessPiece & Chessboard part 1

---
+++ src/FChess.Core/ChessPiece.fs
@@ -0,0 +1,36 @@
+﻿module FChess.Core.ChessPiece
+
+type Side =
+    | White
+    | Black
+
+type ChessRow = 
+    | One = 1 
+    | Two = 2 
+    | Three = 3 
+    | Four = 4 
+    | Five = 5 
+    | Six = 6 
+    | Seven = 7 
+    | Eight = 8
+
+type ChessColumn =
+    | A = 1
+    | B = 2
+    | C = 3
+    | D = 4
+    | E = 5
+    | F = 6
+    | G = 7
+    | H = 8
+
+type ChessPieceInfo = { Side : Side; Coords : ChessRow * ChessColumn }
+
+type ChessPiece =
+    | None of Side
+    | Pawn of Side
+    | Knight of Side
+    | Bishop of Side
+    | Rook of Side
+    | Queen of Side
+    | King of Side
---
+++ src/FChess.Core/Chessboard.fs
@@ -0,0 +1,39 @@
+﻿module FChess.Core.Chessboard
+
+open System
+open System.Collections.Generic
+open FChess.Core.ChessPiece
+
+type T = Dictionary<(ChessRow * ChessColumn), ChessPiece>
+
+let private enumToList<'a> = (Enum.GetValues(typeof<'a>) :?> ('a [])) |> Array.toList
+
+let fieldSide coords =
+    let (row, column) = coords
+    let isLight = (int(row) % 2) = (int(column) % 2)
+
+    match isLight with
+    | true -> Side.White
+    | _ -> Side.Black
+
+let create () =
+
+    let rows = enumToList<ChessRow>
+    let columns = enumToList<ChessColumn>
+    let chessboard = new T(64)
+
+    let getInitialPiece coords =
+        let (row, column) = coords
+        let side = fieldSide coords
+
+        if List.contains row [1;2;7;8] then
+            
+            
+            ChessPiece.None Side.Black
+        else
+            ChessPiece.None side
+
+
+    for r in rows do
+        for c in columns do
+            chessboard.Add((r, c), (int(r), int(c)) |> getInitialPiece)
